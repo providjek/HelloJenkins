5096cdca1a8414c46e8560ee44cf73c70ddf1d0c
Providence Djekoundakom Dingmwal <107733542+providjek@users.noreply.github.com>
2024-09-21 17:54:20 +0100

    Update Main.java

--- src/main/java/org/ucac/icam/Main.java
+++ src/main/java/org/ucac/icam/Main.java
@@ -1,7 +1,25 @@
-package org.ucac-icam;
+package org.ucac.icam;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("*******************************************");
+        dessinerUCACICAM();
+        System.out.println("***********HELLLO JJENKINS***************");
+
     }
+
+    public static void dessinerUCACICAM() {
+        String[] pattern = {
+                "U   U  CCCCC   AAAAA   CCCCC      III  CCCCC   AAAAA  M   M",
+                "U   U  C       A   A   C           I   C       A   A  MM MM",
+                "U   U  C       AAAAA   C    ---    I   C       AAAAA  M M M",
+                "U   U  C       A   A   C           I   C       A   A  M   M",
+                " UUU   CCCCC   A   A   CCCCC      III  CCCCC   A   A  M   M"
+        };
+
+        for (String ligne : pattern) {
+            System.out.println(ligne);
+        }
+    }
+
 }
